a0fef0da5ea316408b3332f36c04fcb5c6f65ee6
Alvaro Condori <con21033@byui.edu>
2024-06-02 18:00:57 -0300

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type {Metadata} from "next";
 import {Poppins} from "next/font/google";
+
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -23,9 +26,9 @@ export default function RootLayout({
     <html lang='en'>
       <body className={poppins.className}>
         <main className='font-normal'>
-          {/* Header */}
+          <Header />
           {children}
-          {/* Footer */}
+          <Footer />
         </main>
       </body>
     </html>
